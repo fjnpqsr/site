f7069ad92ed56064f1fd50304c8f6a2baa9358e9
qsr01271539 <qsr01271539@alibaba-inc.com>
2022-08-29 16:54:46 +0800

    feat(trunk path): logic add

--- src/constant/menu.ts
+++ src/constant/menu.ts
@@ -1,17 +1,13 @@
 /*
  * @Author: Qiu Shao Rong
  * @Date: 2022-08-03 14:12:09
- * @LastEditTime: 2022-08-29 15:23:18
+ * @LastEditTime: 2022-08-29 16:54:01
  * @LastEditors: Qiu Shao Rong
  * @Description:
  * @FilePath: \front-end\src\constant\menu.ts
  */
 
 const menusData = [
-  {
-    label: "Home",
-    key: "/portal",
-  },
   {
     label: "生成trunk分支文件路径",
     key: "/portal/buildTrunkPath",
