3475849e059d2de2277466a70934e65ca863f534
qsr01271539 <qsr01271539@alibaba-inc.com>
2022-08-30 14:42:19 +0800

    feat(i18n maker): add i18n maker page

--- src/constant/menu.ts
+++ src/constant/menu.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Qiu Shao Rong
  * @Date: 2022-08-03 14:12:09
- * @LastEditTime: 2022-08-29 16:54:01
+ * @LastEditTime: 2022-08-30 14:33:16
  * @LastEditors: Qiu Shao Rong
  * @Description:
  * @FilePath: \front-end\src\constant\menu.ts
@@ -11,6 +11,10 @@ const menusData = [
   {
     label: "生成trunk分支文件路径",
     key: "/portal/buildTrunkPath",
+  },
+  {
+    label: "生成多语言脚本",
+    key: "/portal/i18nSqlMaker",
   },
 ];
 
