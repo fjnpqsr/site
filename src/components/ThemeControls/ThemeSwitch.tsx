2fb7e3e5efe26e76124433d1911f4efce95a4615
fjnpqsr <345239925@qq.com>
2024-01-20 22:22:51 +0800

    fix(keep-alive): fixed theme change theme token error
    clear keep-alive cache when theme change

--- src/components/ThemeControls/ThemeSwitch.tsx
+++ src/components/ThemeControls/ThemeSwitch.tsx
@@ -2,12 +2,16 @@ import { Switch } from 'antd';
 import React, { useContext } from 'react';
 
 import { context } from '@/context/context';
+import { useAliveController } from 'react-activation';
 
 const ThemeSwitch = () => {
 	const { state, updateContext } = useContext(context);
+	const { clear } = useAliveController();
+
 	const handleSwitch = (checked: boolean) => {
 		const themeSlogan = checked ? 'light' : 'dark';
 		updateContext({ type: 'theme', payload: themeSlogan });
+		clear();
 	};
 	return (
 		<Switch
