c894bbf77fc9e529fa14e0873c15a30590163d47
qiu.shaorong <qiu.shaorong3@iwhalecloud.com>
2024-05-09 17:13:40 +0800

    feat(color definition): add color definition page

---
+++ src/pages/portal/CMP/ColorDefine/data.ts
@@ -0,0 +1,92 @@
+export const groups = [
+	{
+		label: '主题色',
+		colors: [
+			{ color: '#2684C2', desc: '主题色-蓝' },
+			{ color: '#86bb47', desc: '主题色-绿' },
+			{ color: '#faad14', desc: '功能色-warning' },
+			{ color: '#389e0d', desc: '功能色-success' },
+			{ color: '#096dd9', desc: '功能色-info' },
+			{ color: '#d93026', desc: '功能色-error' },
+		],
+	},
+	{
+		label: '背景色',
+		colors: [
+			{ color: '#fff', desc: '背景色' },
+			{ color: '#f0f2f5', desc: 'forgetPassword背景色' },
+			{ color: '#f5f5f5', desc: '背景色' },
+		],
+	},
+	{
+		label: '字体色',
+		colors: [
+			{ color: 'rgba(0, 0, 0, 0.85)', desc: '字体颜色' },
+			{ color: 'rgba(0, 0, 0, 0.65)', desc: '字体颜色' },
+			{ color: 'rgba(0, 0, 0, 0.45)', desc: '字体颜色' },
+			{ color: 'rgba(0, 0, 0, 0.25)', desc: '字体颜色' },
+			{ color: '#888888', desc: 'Cloud Service Catalog > Cloud Service 卡片副标题' },
+			{ color: '#fff', desc: '字体色' },
+			{ color: '#C1C6CA', desc: 'objectives-tags' },
+		],
+	},
+	{
+		label: '图标颜色',
+		colors: [
+			{color: '#2684C2', desc: 'Cloud Service Catalog > Resource Parameters > Subnet/Auto Scaling 可点颜色'},
+			{color: 'rgb(170, 170, 170)', desc: 'Cloud Service Catalog > Resource Parameters > Subnet/Auto Scaling 不可点颜色'},
+			{color: '#ff9000', desc: 'My Approval Reject Alert icon, 疑问: 是否可以直接使用系统的warning色#faad14'}
+
+		],
+	},
+	{
+		label: '边框色',
+		colors: [
+			{ color: '#d9d9d9', desc: '边框颜色' },
+			{ color: '#dedede', desc: '边框颜色1' },
+			{ color: '#ccc', desc: '边框颜色2' },
+			{ color: '#2684C2', desc: '边框颜色3' },
+			{ color: '#C1C6CA', desc: '边框颜色4' },
+			{ color: '#cdcdcd', desc: '边框颜色5' },
+			{ color: 'rgba(0,0,0,.06)', desc: '边框颜色6' },
+			{ color: '#87bc54', desc: '边框颜色7' },
+			{ color: '#e3e3e3', desc: '边框颜色8' },
+			{ color: '#f5f5f5', desc: '边框颜色9' },
+			{ color: '#D93026', desc: '蓝图边框颜色1' },
+			{ color: '#FAAD14', desc: '蓝图边框颜色2' },
+			{ color: '#389E0D', desc: '蓝图边框颜色3' },
+			{ color: '#2684C2', desc: '蓝图边框颜色4' },
+			{ color: '#1890FF', desc: '蓝图边框颜色5' },
+			{ color: '#239edd', desc: '蓝图边框颜色6' },
+			{ color: '#113B57', desc: '蓝图边框颜色7' },
+		],
+	},
+	{
+		label: '通知模板颜色',
+		colors: [
+			{ color: '#F3F7FA', desc: '头部背景色/底部背景色' },
+			{ color: '#2684C2', desc: '底部字体颜色' },
+			{ color: '#eaeaeb', desc: '邮件底色' },
+			{ color: '#8c8c8c', desc: '字体色（浅色），适配富文本的颜色' },
+			{ color: '#262626', desc: '字体色（深色），适配富文本的颜色' },
+		],
+	},
+	{
+		label: 'Flavor卡片配色',
+		colors: [
+			{ color: '#ccc', desc: '未激活时的边框色' },
+			{ color: '#F7FBFE', desc: '未激活时的背景色' },
+			{ color: '#2684C2', desc: '激活时的边框色' },
+			{ color: '#F0F2F5', desc: '激活时的背景色' },
+			{ color: '#fff', desc: '箭头背景色' },
+			{ color: '#rgba(0, 0, 0, 0.85)', desc: '箭头颜色(继承默认字体色)' },
+		],
+	},
+	{
+		label: '进度条颜色',
+		colors: [
+			{color: '#da2c2c', desc: 'Business Group > Resource Quota > Usage Rate'},
+			{color: '#656ae6', desc: 'Business Group > Resource Quota > Usage Rate'},
+		]
+	}
+];
--- src/pages/portal/CMP/ColorDefine/index.tsx
+++ src/pages/portal/CMP/ColorDefine/index.tsx
@@ -1,81 +1,50 @@
 import PageContainer from '@/components/PageContainer';
-import { ColorPicker, Space } from 'antd';
+import { ColorPicker, Space, Collapse, theme } from 'antd';
 import React from 'react';
+import { groups } from './data';
+
 
 
 const ColorDefine = () => {
+	const { token } = theme.useToken();
 
-	const groups = [
-		{
-			label: '主题色',
-			colors: [
-				{ color: '#2684C2', desc: '主题色-蓝' },
-				{ color: '#86bb47', desc: '主题色-绿' },
-				{ color: '#faad14', desc: '功能色-warning' },
-				{ color: '#389e0d', desc: '功能色-success' },
-				{ color: '#096dd9', desc: '功能色-info' },
-				{ color: '#d93026', desc: '功能色-error' },
-			]
-		},
-		{
-			label: '背景色',
-			colors: [
-				{ color: '#fff', desc: '背景色' },
-				{ color: '#f0f2f5', desc: 'forgetPassword背景色' },
-				{ color: '#f5f5f5', desc: '背景色' },
-			]
-		},
-		{
-			label: '字体色',
-			colors: [
-				{ color: '#000000d9', desc: '字体颜色' },
-				{ color: '#fff', desc: '字体色' },
-			]
-		},
-		{
-			label: '边框色',
-			colors: [
-				{ color: '#d9d9d9', desc: '边框颜色' },
-				{ color: '#dedede', desc: '边框颜色1' },
-				{ color: '#ccc', desc: '边框颜色2' },
-				{ color: '#2684C2', desc: '边框颜色3' },
-				{ color: '#C1C6CA', desc: '边框颜色4' },
-				{ color: '#cdcdcd', desc: '边框颜色5' },
-				{ color: '#0000000f', desc: '边框颜色6' },
-				{ color: '#87bc54', desc: '边框颜色7' },
-				{ color: '#e3e3e3', desc: '边框颜色8' },
-				{ color: '#f5f5f5', desc: '边框颜色9' },
-				{ color: '#D93026', desc: '蓝图边框颜色1' },
-				{ color: '#FAAD14', desc: '蓝图边框颜色2' },
-				{ color: '#389E0D', desc: '蓝图边框颜色3' },
-				{ color: '#2684C2', desc: '蓝图边框颜色4' },
-				{ color: '#1890FF', desc: '蓝图边框颜色5' },
-				{ color: '#239edd', desc: '蓝图边框颜色6' },
-				{ color: '#113B57', desc: '蓝图边框颜色7' },
-			]
-		},
-	];
+	const panelStyle: React.CSSProperties = {
+		marginBottom: 24,
+		background: token.colorFillAlter,
+		borderRadius: token.borderRadiusLG,
+		border: 'none',
+	};
+
+	const items = groups.map(item => {
+		const children = 	<Space wrap direction='vertical'style={{width: '100%'}}>
+			{
+				item.colors.map((item: { color: string, desc: string }) => (
+					<div 
+						key={item.color} 
+						style={{ display: 'flex',  alignItems: 'center', justifyContent:'space-between' }}
+					>
+						<ColorPicker value={item.color} showText />
+						<p>{item.desc}</p>
+					</div>
+				))
+			}
+		</Space>;
+		return {
+			key: item.label,
+			label: item.label,
+			children,
+			style: panelStyle
+		};
+	});
 
 	return (
 		<PageContainer>
-			{groups.map(item => (
-				<div key={item.label}>
-					<p>{item.label}</p>
-					<Space wrap>
-						{
-							item.colors.map((item: { color: string, desc: string }) => (
-								<div key={item.color} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-									<div>
-										<ColorPicker value={item.color} showText />
-									</div>
-									<p>{item.desc}</p>
-								</div>
-							))
-						}
-					</Space>
-				</div>
-			))}
-			
+			<Collapse 
+				items={items}
+				bordered={false}
+				defaultActiveKey={groups.map(item => item.label)}
+				style={{ background: token.colorBgContainer }}
+			/>
 		</PageContainer>
 	);
 };
