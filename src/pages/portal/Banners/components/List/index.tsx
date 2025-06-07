f647bbceed0b200a6f2ac89025f71f41d9b3c20e
fjnpqsr <345239925@qq.com>
2025-06-07 11:55:32 +0800

    feat: add banner status

--- src/pages/portal/Banners/components/List/index.tsx
+++ src/pages/portal/Banners/components/List/index.tsx
@@ -1,21 +1,22 @@
-import { List, Image, theme } from 'antd';
+import { List, Image, theme, Badge } from 'antd';
 import React, { useState } from 'react';
 import css from './index.module.less';
 import { IBanner } from '../../hooks/useBanner';
 
-
 interface IBannerList {
-	data?:IBanner[];
-	selected: IBanner | null;
-	onClick: any;
-	loading?: boolean
+    data?: IBanner[];
+    selected: IBanner | null;
+    onClick: any;
+    loading?: boolean;
 }
 
 export default function BannerList(props: IBannerList) {
-	const {selected, onClick, data, loading} = props;
+	const { selected, onClick, data, loading } = props;
 	const [pageSize, setPageSize] = useState<any>(4);
-	const {token: {colorPrimary}} = theme.useToken();
-    
+	const {
+		token: { colorPrimary },
+	} = theme.useToken();
+
 	return (
 		<List
 			loading={loading}
@@ -33,28 +34,33 @@ export default function BannerList(props: IBannerList) {
 			}}
 			dataSource={data}
 			renderItem={(item) => (
-				<List.Item
-					className={item.id === selected?.id? css.selected: ''}
-					style={{borderLeftColor: colorPrimary,cursor: 'pointer'}}
-					key={item.title}
-					onClick={() => {
-						onClick({...item, type: 'edit'});
-					}}
-					extra={
-						<Image
-							width={124}
-							height={70}
-							src={item.url}
-						/>
-					}
-				>
-					<List.Item.Meta
-						title={item.title}
-					/>
-					<span style={{whiteSpace: 'nowrap',width: '100%', display: 'inline-block', overflow:'hidden', textOverflow: 'ellipsis'}}>
-						{item.subtitle}
-					</span>
-				</List.Item>
+				<Badge.Ribbon text={item.status === '1'? '启用' : '禁用'} color={item.status === '1'? 'green' : 'volcano'}>
+					<List.Item
+						className={item.id === selected?.id ? css.selected : ''}
+						style={{
+							borderLeftColor: colorPrimary,
+							cursor: 'pointer',
+						}}
+						key={item.title}
+						onClick={() => {
+							onClick({ ...item, type: 'edit' });
+						}}
+						extra={<Image width={124} height={70} src={item.url} />}
+					>
+						<List.Item.Meta title={item.title} />
+						<span
+							style={{
+								whiteSpace: 'nowrap',
+								width: '100%',
+								display: 'inline-block',
+								overflow: 'hidden',
+								textOverflow: 'ellipsis',
+							}}
+						>
+							{item.subtitle}
+						</span>
+					</List.Item>
+				</Badge.Ribbon>
 			)}
 		/>
 	);
